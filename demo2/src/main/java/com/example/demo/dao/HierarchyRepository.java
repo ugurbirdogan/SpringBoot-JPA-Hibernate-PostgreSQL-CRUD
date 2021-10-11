3d08482f471338aa830ae5a755bbd63a0b611f3b
ugurbirdogan <41667882+ugurbirdogan@users.noreply.github.com>
2021-10-11 10:24:08 +0300

    Crud operations with postgresql using spring boot and spring data with jpa hibernate.

--- demo2/src/main/java/com/example/demo/dao/HierarchyRepository.java
+++ demo2/src/main/java/com/example/demo/dao/HierarchyRepository.java
@@ -2,8 +2,8 @@ package com.example.demo.dao;
 
 import com.example.demo.model.Hierarchy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface HierarchyRepository extends JpaRepository<Hierarchy, Long> {
 }
